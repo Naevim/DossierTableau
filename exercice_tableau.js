cd1eddd6e29ef34d3cad2428bf2c6067966bf5d3
Naevim <146922198+Naevim@users.noreply.github.com>
2023-10-08 11:36:55 +0200

    Add files via upload

---
+++ exercice_tableau.js
@@ -0,0 +1,10 @@
+//Tableau 
+
+let TableauNames=[];
+TableauNames.push("Vincent");
+TableauNames.push("Paul");
+TableauNames.push("Arthur");
+
+for (let i=0;i<TableauNames.length;i++) {
+    console.log(TableauNames[i] + " va a la peche");
+}
